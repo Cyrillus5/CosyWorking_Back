aabef4dc966596642cd63069db31afeccb214bbd
Corentin <corentin.vnqr@gmail.com>
2022-10-12 15:12:12 +0200

    fixed(validation): workspace

--- app/validation/schemas/workspace/workspaceSearchSchema.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('joi');
-
-module.exports = Joi.object({
-    city: Joi.string(),
-    start_day: Joi.string(),
-    end_day: Joi.string(),
-    equipment: Joi.string()
-}).required();
--- app/validation/schemas/workspace/workspaceStateUpdateSchema.js
+++
@@ -1,6 +0,0 @@
-const Joi = require('joi');
-
-module.exports = Joi.object({
-    state: Joi.string().required(),
-}).required();
-
