47bcabc3ef15eda6b8aaa761e686b1067f0d024b
Corentin <corentin.vnqr@gmail.com>
2022-10-12 12:02:27 +0200

    build(validation): add schema on profil + started workspace

---
+++ app/validation/schemas/workspace/workspaceUpdateSchema.js
@@ -0,0 +1,16 @@
+// const Joi = require('joi');
+
+// module.exports = Joi.object({
+//     title: Joi.string(),
+//     description: Joi.string(),
+//     address: Joi.string(),
+//     zip_code: Joi.string(),
+//     city: Joi.string(),
+//     day_price: Joi.number(),
+//     half_day_price: Joi.number(),
+//     user_id: Joi.number(),
+//     equipment_list: Joi.object({
+//         description: Joi.string()
+//     }),
+// }).required();
+
